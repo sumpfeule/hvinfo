c1938dd53207c5713c81b492c4821ef17c2a226c
Marcel Rupp <comfix@comfix.cc>
2022-06-14 13:15:28 +0200

    Redone file parsing
    .. so it works on RedHat as well as Debian

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, fs, io, process};
+use serde_json;
+use std::{collections::HashMap, io::Read, process::exit};
 
 const HVPATH: &str = "/var/lib/hyperv/.kvp_pool_3";
 
@@ -8,48 +9,65 @@ const HVLEN: usize = 40960;
 const HV_KVP_EXCHANGE_MAX_KEY_SIZE: usize = 512;
 const HV_KVP_EXCHANGE_MAX_VALUE_SIZE: usize = 2048;
 
-const MAX_INDEX: usize = HVLEN / (HV_KVP_EXCHANGE_MAX_KEY_SIZE + HV_KVP_EXCHANGE_MAX_VALUE_SIZE);
-
-fn read_hvfile() -> Result<String, io::Error> {
-    fs::read_to_string(HVPATH)
-}
-
-fn kv_at_index(hvdata: &String, index: usize) -> (String, String) {
-    assert!(index <= MAX_INDEX, "index out of bounds");
-
-    let k_start: usize = index * (HV_KVP_EXCHANGE_MAX_KEY_SIZE + HV_KVP_EXCHANGE_MAX_VALUE_SIZE);
-    let v_start: usize = k_start + HV_KVP_EXCHANGE_MAX_KEY_SIZE;
-
-    let k_end: usize = k_start + HV_KVP_EXCHANGE_MAX_KEY_SIZE;
-    let v_end: usize = v_start + HV_KVP_EXCHANGE_MAX_VALUE_SIZE;
-    (
-        hvdata[k_start..k_end].to_string().replace("\0", r#""#),
-        hvdata[v_start..v_end].to_string().replace("\0", r#""#),
-    )
-}
-
 fn main() {
-    let hvdata = match read_hvfile() {
-        Ok(s) => s,
-        Err(e) => {
-            println!("Unable to open {}: {}", HVPATH, e.to_string());
-            process::exit(1);
+    let f = std::fs::File::open(HVPATH).unwrap();
+    let mut reader = std::io::BufReader::new(f);
+    let mut read_bytes = 0;
+
+    let mut buf_key: [u8; HV_KVP_EXCHANGE_MAX_KEY_SIZE] = [0u8; HV_KVP_EXCHANGE_MAX_KEY_SIZE];
+    let mut buf_value: [u8; HV_KVP_EXCHANGE_MAX_VALUE_SIZE] = [0u8; HV_KVP_EXCHANGE_MAX_VALUE_SIZE];
+
+    let mut kv_pairs: HashMap<String, String> = Default::default();
+
+    while read_bytes < HVLEN {
+        match reader.read_exact(&mut buf_key) {
+            Ok(()) => {
+                read_bytes += HV_KVP_EXCHANGE_MAX_KEY_SIZE;
+                let key_size: usize = match buf_key.iter().position(|n| n == &0) {
+                    Some(n) => n,
+                    None => HV_KVP_EXCHANGE_MAX_KEY_SIZE,
+                };
+
+                match reader.read_exact(&mut buf_value) {
+                    Ok(()) => {
+                        read_bytes += HV_KVP_EXCHANGE_MAX_VALUE_SIZE;
+                        let value_size: usize = match buf_value.iter().position(|n| n == &0) {
+                            Some(n) => n,
+                            None => HV_KVP_EXCHANGE_MAX_VALUE_SIZE,
+                        };
+
+                        let key = match String::from_utf8(buf_key[..key_size].to_vec()) {
+                            Ok(s) => s,
+                            Err(e) => {
+                                eprintln!("{}", e);
+                                exit(1);
+                            }
+                        };
+
+                        let value = match String::from_utf8(buf_value[..value_size].to_vec()) {
+                            Ok(s) => s,
+                            Err(e) => {
+                                eprintln!("{}", e);
+                                exit(1);
+                            }
+                        };
+
+                        kv_pairs.insert(key, value);
+                    }
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        exit(1);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+                exit(1);
+            }
         }
-    };
-    if hvdata.len() != HVLEN {
-        println!(
-            "{} seems to have the wrong size ({}, expected {}).\nIncompatible hyper-V version?",
-            HVPATH,
-            hvdata.len(),
-            HVLEN
-        );
-        process::exit(1);
     }
 
-    let mut hvinfo: HashMap<String, String> = HashMap::new();
-    for i in 0..MAX_INDEX {
-        let (k, v) = kv_at_index(&hvdata, i);
-        hvinfo.insert(k, v);
-    }
-    println!("{:?}", hvinfo);
+    assert_eq!(HVLEN, read_bytes);
+
+    println!("{}", serde_json::to_string(&kv_pairs).unwrap());
 }
